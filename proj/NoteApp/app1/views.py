5f660174ba91ef2b115148482382147eed4fc715
Mavera07 <cyberoyal@windowslive.com>
2017-05-07 11:35:16 +0300

    SERVER TEMPLATE PROJECT

--- proj/NoteApp/app1/views.py
+++ proj/NoteApp/app1/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 
 def index(request):
 
 
-    context = {"var1":"helloo"}
+    context = {"var1":"","var2":settings.BASE_DIR}
     return render(request, 'index.html', context)

107ff7d68607312a42b7d9668bf0a0f8b05fcc3b
Mavera07 <cyberoyal@windowslive.com>
2017-05-08 11:14:06 +0300

    NoteApp visjs is being used

--- proj/NoteApp/app1/views.py
+++ proj/NoteApp/app1/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.conf import settings
 
+from os import listdir
+from os.path import isfile, join
+
+from os import walk
+
 # Create your views here.
 
 def index(request):
+   
+    # TODO focusPath will be updated by using request
+    focusPath = settings.BASE_DIR+"/storage/data/0"
+    focusInfoPath = focusPath+"/.init.noteapp"
 
+    focusFiles = [];focusDirs = [];
+    for (dirpath, dirnames, filenames) in walk(focusPath):
+        focusFiles.extend(filenames); focusFiles.remove('.init.noteapp')
+        focusDirs.extend(dirnames)
+        break
 
-    context = {"var1":"","var2":settings.BASE_DIR}
+    focusInfo = []
+    with open(focusInfoPath,'r') as ff:
+        ff.readline(); focusInfo.append(ff.readline()); ff.readline()
+        ff.readline(); focusInfo.append(ff.readline()); ff.readline()
+
+    context = {"focusPath":focusPath,"focusName":focusInfo[0].strip(),"focusDirs":focusDirs, "focusFiles":focusFiles }
     return render(request, 'index.html', context)

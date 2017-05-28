1f04a57e503f0b9bd957ce12dab1395553c29f3b
Mavera07 <cyberoyal@windowslive.com>
2017-05-28 12:09:14 +0300

    The feature "custom locations for nodes" is implemented

--- proj/NoteApp/app1/views.py
+++ proj/NoteApp/app1/views.py
@@ -4,6 +4,7 @@ from django.conf import settings
 import os
 import shutil
 import re
+import textwrap
 
 # Create your views here.
 
@@ -64,6 +65,21 @@ def ajax(request):
         focusPath = request.GET['path']
         focusFullPath = settings.BASE_DIR + "/storage/data/" + focusPath
 
+        with open(focusFullPath+"/.locations.noteapp" ,'r') as ff:
+            locInfo = ''.join(ff.readlines())
+
+        locInfoUpdated = re.search(r'(.+\}).+?$',locInfo,re.DOTALL).group(1)+","+textwrap.dedent("""
+          {
+            "x": 40,
+            "y": 40,
+            "id": "%d"
+          }
+        ]""" % (len(re.findall('"id"',locInfo)) + 1) )
+
+        with open(focusFullPath+"/.locations.noteapp" ,'w') as ff:
+            ff.write(locInfoUpdated)
+        
+
         foldername = 0
         while(os.path.isdir(focusFullPath+"/"+str(foldername))):
             foldername += 1
@@ -75,6 +91,16 @@ def ajax(request):
             ff.write("\n")
         with open(newnodeFullPath+"/.notes.noteapp",'w') as ff:
             ff.write("")
+        with open(newnodeFullPath+"/.locations.noteapp",'w') as ff:
+            ff.write(
+             """[
+                    {
+                        "x": 0,
+                        "y": 0,
+                        "id": "1"
+                    }
+                ]"""
+            )
 
     elif 'deletenode' in request.GET and 'path' in request.GET:
         focusPath = request.GET['path']
@@ -82,6 +108,20 @@ def ajax(request):
 
         shutil.rmtree(focusFullPath)
 
+        ## Remove node position from parent view
+        parentPath = re.search("(.+)\/.+?",focusPath).group(1)
+        parentFullPath = settings.BASE_DIR + "/storage/data/" + parentPath
+
+        with open(parentFullPath+"/.locations.noteapp" ,'r') as ff:
+            locInfo = ''.join(ff.readlines())
+
+        tt = re.search(r'(.+)\{.+?\}(.+?$)',locInfo,re.DOTALL)
+        tt2 = re.search(r'(.+),.*?$',tt.group(1),re.DOTALL)
+        locInfo = tt2.group(1)+tt.group(2)
+
+        with open(parentFullPath+"/.locations.noteapp" ,'w') as ff:
+            ff.write(locInfo)
+            
     elif 'editnode' in request.GET and 'path' in request.GET and 'name' in request.GET:
         focusPath = request.GET['path']
         focusFullPath = settings.BASE_DIR + "/storage/data/" + focusPath
